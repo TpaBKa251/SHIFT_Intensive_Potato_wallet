df7ed7ccdb66e486c8eac6a16e686c7f8aa8ab87
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2024-07-06 00:30:53 +0700

    Создал пустые классы моеделей и контроллеров

--- src/main/java/ru/cft/template/WalletApplication.java
+++ src/main/java/ru/cft/template/WalletApplication.java
@@ -8,7 +8,6 @@ public class WalletApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WalletApplication.class, args);
-
 	}
 
 }
--- src/main/java/ru/cft/template/api/UserController.java
+++ src/main/java/ru/cft/template/api/UserController.java
@@ -1,2 +1,40 @@
-package ru.cft.template.api;public class UserController {
+package ru.cft.template.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.cft.template.model.User;
+
+@RequestMapping("/potato/api/users")
+@Slf4j
+public class UserController {
+
+    @GetMapping("/{userID}")
+    public User getByID(@PathVariable int userID){
+        log.info("Success");
+        return null;
+    }
+
+    @GetMapping("/findByEmail")
+    public User getByEmail(@PathVariable String email){
+        log.info("Success");
+        return null;
+    }
+
+    @GetMapping("/findByPhoneNumber")
+    public User getByPhoneNumber(@PathVariable String phoneNumber){
+        log.info("Success");
+        return null;
+    }
+
+    @PostMapping
+    public User create(){
+        log.info("Success");
+        return null;
+    }
+
+    @PatchMapping
+    public User update(@PathVariable int userID){
+        log.info("Success");
+        return null;
+    }
 }
