ea623c330861c217ed050bbc12be1e9d0926c950
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2024-07-11 15:50:34 +0700

    Попытки

--- src/main/java/ru/cft/template/repository/BannedTokenRepository.java
+++ src/main/java/ru/cft/template/repository/BannedTokenRepository.java
@@ -1,10 +1,12 @@
 package ru.cft.template.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.cft.template.entity.BannedToken;
 
 import java.util.Optional;
 
+@Repository
 public interface BannedTokenRepository extends JpaRepository<BannedToken, String> {
     Optional<BannedToken> findByToken(String token);
 }
