18290f65b2c7b424f670721a81a58d528389af04
Лапшин Илья Романович <irl1@tpu.ru>
2024-08-30 01:15:40 +0700

    Эксперимент с токеном продолжается. Теперь ВОЗМОЖНО он будет автоматом баниться

--- src/main/java/ru/cft/template/repository/BannedTokenRepository.java
+++ src/main/java/ru/cft/template/repository/BannedTokenRepository.java
@@ -3,6 +3,7 @@ package ru.cft.template.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.cft.template.entity.BannedToken;
@@ -15,6 +16,6 @@ public interface BannedTokenRepository extends JpaRepository<BannedToken, String
 
     @Modifying
     @Transactional
-    @Query(value = "INSERT INTO banned_tokens (token) VALUES (:token) ON CONFLICT (token) DO NOTHING", nativeQuery = true)
-    void insertIfNotExist(BannedToken bannedToken);
+    @Query(value = "INSERT INTO banned_tokens (token) VALUES (:#{#bannedToken.token}) ON CONFLICT (token) DO NOTHING", nativeQuery = true)
+    void insertIfNotExist(@Param("bannedToken") BannedToken bannedToken);
 }
